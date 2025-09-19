885f121aa39345ba1e295faf087349e5ae81e5df
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-09-19 16:58:06 +0200

    [~] Added first logic for generate password logic and check the strength of password

---
+++ PasswordLogic.py
@@ -0,0 +1,34 @@
+import secrets
+import string
+import random
+
+
+def generate_password(length=12, use_uppercase=True, use_digits=True, use_special=True):
+
+    character_set = string.ascii_lowercase
+
+    if use_uppercase:
+        character_set += string.ascii_uppercase
+    if use_digits:
+        character_set += string.digits
+    if use_special:
+        character_set += string.punctuation
+
+    if not character_set:
+        raise ValueError("At least one character set must be selected.")
+
+    password = ''.join(secrets.choice(character_set) for _ in range(length))
+
+    return password
+
+
+def check_strength(password):
+    length = len(password)
+    strength = "Very Weak"
+
+    if length >= 8:
+        strength = "Weak"
+    if length >= 12:
+        strength = "Medium"
+
+    return strength
